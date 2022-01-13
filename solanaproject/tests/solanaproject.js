57ad6495a51b3f1215ce03af72c60008f2e7e4f4
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-13 14:52:59 +0200

    basic solana program and test

--- solanaproject/tests/solanaproject.js
+++ solanaproject/tests/solanaproject.js
@@ -1,14 +1,32 @@
 const anchor = require('@project-serum/anchor');
 
-describe('solanaproject', () => {
+const main = async () => {
+	console.log("🚀 Starting test...");
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+	/*
+		First, we tell Anchor to set our provider. So, it gets this data from solana config get. 
+		In this case, it's grabbing our local environment! This way Anchor knows to run our code 
+		locally (later we'll be able to test our code on devnet!).
+	*/
+	anchor.setProvider(anchor.Provider.env());
+	/*
+		Then, we grab anchor.workspace.Myepicproject and this is a super cool thing given to us by 
+		Anchor that will automatically compile our code in lib.rs and get it deployed locally on a 
+		local validator. A lot of magic in one line and this is a big reason Anchor is awesome.
+	*/
+	const program = anchor.workspace.Solanaproject;
+	const tx = await program.rpc.startStuffOff();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Solanaproject;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+	console.log("📝 Your transaction signature", tx);
+}
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+
+runMain();
